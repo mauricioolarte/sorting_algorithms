687dc28f522585b7dcdf03d3d08081c735f67ed4
mauricioolarte <reneolarte@yahoo.com>
2020-06-16 02:13:51 +0000

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
-
+/**
+ *bubble_sort - this function sort a array.
+ *@array: array whit integer to sort.
+ *@size: size of array.
+ */
 void bubble_sort(int *array, size_t size)
 {
 	unsigned int count = 1, temp, i;
@@ -11,7 +15,7 @@ void bubble_sort(int *array, size_t size)
 	while (count)
 	{
 		count = 0;
-		for (i = 0; i < size -1; i++)
+		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{

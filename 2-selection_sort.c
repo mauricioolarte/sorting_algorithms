57adf7c522a41db066008c7a61850141475a1253
mauricioolarte <reneolarte@yahoo.com>
2020-06-16 08:37:53 +0000

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -20,18 +20,20 @@ void selection_sort(int *array, size_t size)
 			if (array[i] < min)
 			{
 				min = array[i];
-/*				printf(" min %i a[%i]\n", min, i);*/
+				printf(" min %i a[%i]\n", min, i);
 				min_i = i;
 				flag = 1;
 			}
 		}
+		if (flag == 1)
+		{
 			temp = array[counter];
 			array[counter] = array[min_i];
 			array[min_i] = temp;
-			counter++;
-			if (flag == 1)
-				print_array(array, size);
-			flag = 0;
+			print_array(array, size);
+		}
+		counter++;
+		flag = 0;
 	}
 
 }
